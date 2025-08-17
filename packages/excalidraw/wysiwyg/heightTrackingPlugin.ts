a35c78df108079a7452f10b7778db83beb8b1aa7
Sebastian <bochoniuks@gmail.com>
2025-08-17 10:14:56 +0100

    Paragraph has a splitID

--- packages/excalidraw/wysiwyg/heightTrackingPlugin.ts
+++ packages/excalidraw/wysiwyg/heightTrackingPlugin.ts
@@ -139,6 +139,17 @@ export const HeightTracking = Extension.create({
         //   }
         // },
       },
+      {
+      types: ["paragraph"],
+      attributes: {
+        splitId: {
+          default: null,
+          parseHTML: el => el.getAttribute("data-split-id"),
+          renderHTML: attrs =>
+            attrs.splitId ? { "data-split-id": attrs.splitId } : {},
+        },
+      },
+    },
     ];
   },
   addProseMirrorPlugins() {
